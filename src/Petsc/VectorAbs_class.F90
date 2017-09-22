4a8610dc12b75458a3575f728ebf77fa9ad48de5
mtsodf <mtsodf@gmail.com>
2017-09-22 20:08:52 -0300

    Codigo inicial utilizando o PETSC. Makefile adicionado para compilacao

---
+++ src/Petsc/VectorAbs_class.F90
@@ -0,0 +1,13 @@
+module VectorAbs_class
+implicit none
+ 
+contains
+    
+    subroutine DummySubVector()
+        implicit none
+        
+        write(*,*) "Chamando DummySubVector subrotina." 
+        
+    end subroutine DummySubVector
+
+end module VectorAbs_class
